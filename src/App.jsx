10dd4b523ab27b98e39414bdfe765a9e723d5e8c
jean-siqueira <jean.siqueira@outlook.com.br>
2023-03-13 18:40:19 -0300

    Componente condicionar v1

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,18 @@ import Familia from "./components/basicos/Familia";
 import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import ListaAlunos from "./components/repeticao/ListaAlunos";
 import TabelaProdutos from "./components/repeticao/TabelaProdutos";
+import ParOuImpar from "./components/condicional/ParOuImpar";
 
 export default (_) => (
   <div className="App">
     <h1>Fundamentos React</h1>
 
     <div className="Cards">
+
+    <Card titulo="#08 - Renderização Condicional" color="#982395">
+        <ParOuImpar numero={20}></ParOuImpar>
+      </Card>
+
       <Card titulo="#07 - Tabela Produtos" color="#001f3f">
         <TabelaProdutos></TabelaProdutos>
       </Card>
